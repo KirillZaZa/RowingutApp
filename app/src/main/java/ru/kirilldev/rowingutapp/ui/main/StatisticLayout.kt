66d8938a2561eaffca710ef296ed9136012b2445
Kirill <kirill.189452@gmail.com>
2021-08-23 18:25:55 +0300

    layouts

---
+++ app/src/main/java/ru/kirilldev/rowingutapp/ui/main/StatisticLayout.kt
@@ -0,0 +1,19 @@
+package ru.kirilldev.rowingutapp.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import ru.kirilldev.rowingutapp.R
+
+class StatisticLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttrs: Int = 0
+): ConstraintLayout(context, attrs, defStyleAttrs) {
+
+    init{
+        View.inflate(context, R.layout.statistic_layout, this)
+    }
+
+}
