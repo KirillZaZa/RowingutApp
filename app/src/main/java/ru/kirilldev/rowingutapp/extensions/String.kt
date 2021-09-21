9ef65f70be50b1307ff1393376fa6cd93e6258c3
kirilldev <kirill.189452@gmail.com>
2021-09-21 19:05:14 +0300

    image loader / viewmodel fixes / extensions

--- app/src/main/java/ru/kirilldev/rowingutapp/extensions/String.kt
+++ app/src/main/java/ru/kirilldev/rowingutapp/extensions/String.kt
@@ -4,8 +4,19 @@ import java.security.MessageDigest
 
 
 //email validation
-fun String.checkEmail(email: String): Boolean{
-    return false
+fun String.isEmailValid(email: String): Boolean{
+    val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+    return email.matches(emailPattern.toRegex())
+}
+
+fun String.checkPasswordLength(password: String): Boolean{
+    if(password.length < 6) return false
+    return true
+}
+
+fun String.isPasswordValid(password: String): Boolean{
+    val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\\\S+\$).{4,}\$"
+    return password.matches(passwordPattern.toRegex())
 }
 
 
